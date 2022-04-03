6d2c1de01a7d277b502b912eac276f247640e617
tarek-elhak <tarekroja123@gmail.com>
2022-04-03 09:27:23 +0200

    conditionally rendering the ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,14 @@ import classes from "./Burger.module.css"
 
 const burger = (props) => {
 
-    const ingredients = Object.keys(props.ingredients).map(ingredient => {
+    let ingredients = Object.keys(props.ingredients).map(ingredient => {
         return [...Array(props.ingredients[ingredient])].map((_,index)=>{
             return <BurgerIngredient key={ingredient+index} type={ingredient} />
         })
-    })
+    }).reduce((prevIngredient,currentIngredient) =>
+        prevIngredient.concat(currentIngredient),[])
 
+    ingredients = ingredients.length === 0 ? <p>please start adding ingredients !</p> : ingredients
     return (
         <article className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
