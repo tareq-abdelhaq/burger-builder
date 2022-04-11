b29adc37328aa1315f23483b9f5658596cd71428
tarek-elhak <tarekroja123@gmail.com>
2022-04-11 09:08:59 +0200

    make the logo clickable

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,13 @@
 import classes from "./Logo.module.css";
 import burgerLogo from "../../assets/images/burger-logo.png"
+import {NavLink} from "react-router-dom";
 
 const logo = (props) => (
-    <img src={burgerLogo} alt="Burger Logo" className={classes.Logo}
-         style={{height: props.height}}
-    />
+    <NavLink to="/">
+        <img src={burgerLogo} alt="Burger Logo" className={classes.Logo}
+             style={{height: props.height}}
+        />
+    </NavLink>
 )
 
 export default logo;
